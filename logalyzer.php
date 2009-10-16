89055cc75c2a43c9f999e39312b6143134799005
Tim <tim@timspangler.com>
2009-10-16 18:56:37 -0500

    Started on the get_rip_settings() function to extract information related to drive settings, offset, etc.

--- logalyzer.php
+++ logalyzer.php
@@ -43,12 +43,47 @@ class Logalyzer {
 		if(strpos($this->logfile[0], "extraction logfile")) {
 			return "earlier than .95";
 		} else {
-			$version_string = preg_match('/ V(.+) from/', $this->logfile[0], $matches);
-			
-			//	$matches[0] will contain the whole string in the expression
-			//	$matches[1] will contain just (.+), which is what we want
-			return $matches[1];
+			$version_string = preg_match('/ V(?<version>.+) from/', $this->logfile[0], $matches);
+			if($version_string) {
+				return $matches['version'];
+			} else {
+				return false;
+			}
 		}
+	}
+	
+	function is_new_eac() {
+		$version = $this->get_eac_version();
+		
+		if($version && strpos($version, "earlier than") === false) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	function get_rip_settings() {
+		/*
+			1.	Determine EAC version
+				1a.	If < .99, get the next x lines after "Used drive"
+				1b.	If > .99, get even more lines.
+		*/
+
+		//	Some things are the same in both logs - the used drive line, for example
+		//	The regex grabs everything between "Used drive:" and "Adapter"
+		$used_drive = preg_match('/Used drive\s+:\s(?<drive>.+)\sAdapter/', $this->logfile_whole, $drive_matches);
+		$rip_settings['drive'] = trim($drive_matches['drive']);
+
+		//	Read info: In old EAC, this has everything on one line
+		$read_info = preg_match('/Read mode\s+:\s(?<readmode>.+)/', $this->logfile_whole, $read_matches);
+
+		if(!($this->is_new_eac())) {
+			//	Parse $read_matches as if everything were one line
+		} else {
+			//	Do a few more preg_match()es to get the rest of the read settings
+		}
+		
+		return $rip_settings;
 	}
 	
 	function get_album_info() {
